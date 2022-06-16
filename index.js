2596adcfb2bac351d7ea43f505876860c90a342c
Maksim <maks.s375@gmail.com>
2022-06-16 14:09:00 +0300

    add function arrow

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
-"use strict";
+// "use strict";
 
-function test1(){
-  console.log(this)
-}
+// const sumNum = (...num) => num.reduce((acum, vol) => acum + vol);
 
-const test2 = function(){
-  console.log(this)
-}
+// sumNum(3, 5, 4, 3, 6);
 
-const test3 = ()=> {
-  console.log(this)  
-}
+// const later1 = ['a', 'e', 'o'];
+// const later2 = ['b', 'c','d'];
 
-test1();
-test2();
-test3();
+// const all = [...later1, ...later2];
+
+// console.log(all)
+
+// const factorial = (num, num1) => {
+//   if (num < 0) {
+//     return null;
+//   }
+//   if (num === 1 || num === 0) {
+//     return 1n;
+//   }
+//   return BigInt(num) * factorial(num - 1);
+// };
+// console.log(factorial(5));
+
+// const userInput = myNum;
+// const myNum = 15;
+
+// while (true) {
+//   const userNum = prompt("try to guess my number");
+
+//   if (userNum < myNum) {
+//     console.log("try entering a larger number");
+//   }
+//   if (userNum > myNum) {
+//     console.log("try to enter a smaller number");
+//   }
+
+// console.log("congratulations!!!!");
+// }
+
+// myNum = 25;
+// while (myNum === userNum) {
+//   userNum = prompt("try to guess my number");
+//   // myNum === userNum;
+
+//   if (myNum > userNum) {
+//     console.log("try entering a larger number");
+//   }
+//   if (myNum < userNum) {
+//     console.log("try to enter a smaller number");
+//   }
+// }
+// console.log("congratulations!!!!");
+
+// let bigNum = (a, b) => (a > b ? a : b);
+
+// console.log(bigNum(17, 2));
+
+const str = 'to Be or NoT To bE';
+
+countVowels=word=>word.split("").reduce((c,l)=>(["a","e","i","o","u"].indexOf(l.toLowerCase())+1)?++c:c,0);
+ console.log(countVowels(str))
