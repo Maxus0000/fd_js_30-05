37871eb931e6a0f0497e007f78b15d789107d71a
Maksim <maks.s375@gmail.com>
2022-06-19 22:36:35 +0300

    add exampl Products

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
 "use strict";
-
-class Woker{
-constructor(fname, sname, rate,days){
-  this.fname = fname;
-  this.sname = sname;
-  this.rate = rate;
-  this.days = days;
-}
-
-getSalary(){
-  return this.days*this.rate;
-}
-get days(){
-  return this._days;
-}
-set days(days){
-  if (typeof days !== 'number'){
-    throw new TypeError('Days must be a number');
+const arrCorency = ["uah", "gbr", "usd"];
+class Product {
+  constructor(name, price, corency) {
+    this.name = name;
+    this.price = price;
+    this.corency = corency;
+    this.available = true;
   }
-  if (days <0 || days > MAX_DAYS){
-    throw new RangeError ('day must beb0-' +MAX_DAYS)
+  get price() {
+    return this._price;
   }
-  this._days = days;
+  set price(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("must be number");
+    }
+    if (value > 1000 || value < 0) {
+      throw new RangeError(" must be 0...1000");
+    }
+    this._price = value;
+  }
+  set corency(value) {
+    if (arrCorency.includes(value) === false) {
+      throw new Error(" invalid value ");
+    }
+    this._corency = value;
+  }
+  static isProduct(obj) {
+    return obj instanceof Product;
+  }
 }
+const milk = new Product("korovka", 35, "usd");
+console.log(milk);
+
+class OnlineProduct extends Product {
+  constructor(name, price, corency, delivery) {
+    super(name, price, corency);
+    this.delivery = delivery;
+  }
 }
+
+// создать класс покупатель у которого будет сво-во кэш и метод купить товар( товар - товар и когда хватает денег)
+
+class Buyer {
+  constructor(cash) {
+    this.cash = cash;
+  }
+  getProduct(obj) {
+    if (Product.isProduct(obj)=== false) {
+      throw new TypeError(' not product')
+    }
+    if (obj.price > this.cash) {
+      throw new RangeError ( ' need more money')
+    }
+    if (obj.available === false) {
+      throw new Error(' not availeble');
+    }
+    obj.available = false;
+  }
+}
+
+const buyer = new Buyer(40)
+buyer.getProduct(milk)
+buyer.getProduct(milk)
